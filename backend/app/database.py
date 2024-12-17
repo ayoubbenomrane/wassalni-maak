5538780528df0832197fa150481cf20550d138c8
ayoubban2003 <ayoubbomrane@gmail.com>
2024-12-17 08:42:28 +0100

    live database setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL= 'postgresql://postgres:Ayoub2003@localhost/wassalniv2'
+SQLALCHEMY_DATABASE_URL = 'postgresql://avnadmin:AVNS_9qiRQNKHgF6nAThh1bn@pg-2692ca79-ayoubbomrane-bf7e.k.aivencloud.com:22294/wassalnimaak?sslmode=require'
 engine =create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
